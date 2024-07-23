ca445311bb25ad2e7b207a1eb882a7cf464e9ea0
Janei Araujo <jaaraujo.developer@gmail.com>
2024-07-23 15:40:38 -0300

    alterando nome do projeto

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -217,7 +217,7 @@ const LoggedInLayout = ({ children }) => {
             noWrap
             className={classes.title}
           >
-            WhaTicket
+            InManager
           </Typography>
           {user.id && <NotificationsPopOver />}
 
